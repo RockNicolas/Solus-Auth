5cfd9bb4d74a50e0f6ab0737bb55a78a66998d30
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-03-14 14:12:57 -0300

    Criação do meu controller de authentificação

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+    public function showLoginForme()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'Test' => 'requerid|Test',
+            'senha' => 'requerid',
+        ]);
+
+        $user = User::where('Test', $request->Test)->first();
+
+        if ($user && Hash::check($request->senha, $user->senha)){
+            Auth::login($user);
+            return redirect()->intended('/home');
+        }
+
+        return back()->withErrors([
+            'Test' => 'Nome de usuario inválido.',
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticate
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $table = '';
+
+    public $timestamps = 'true';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'Test',
+        'senha',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'senha',
     ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+        
+    public function getAuthPassword()
+    {
+        return $this->senha;
+    }
 }
